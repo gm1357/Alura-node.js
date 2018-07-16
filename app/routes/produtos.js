c7202ebe06d86919085126fff3570006350ccf48
Gabriel Machado <g_machado1@hotmail.com>
2018-07-16 14:24:15 -0300

    Implementado DAO de produtos

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,8 +1,9 @@
 module.exports = function(app) {
     app.get("/produtos",function(req, res) {
         connection = app.infra.connectionFactory();
+        produtosBanco = new app.infra.ProdutosDAO(connection);
 
-        connection.query('select * from livros', function(err, results){
+        produtosBanco.lista(function(err, results){
             res.render('produtos/lista', {lista: results});
         });
 
